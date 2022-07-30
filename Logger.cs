09c3b51224ee61b17ff66cef5e110011d1900286
CVRKin <110277649+CVRKin@users.noreply.github.com>
2022-07-29 23:22:23 -0700

    🥗
    🥗

---
+++ Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MelonLoader;
+
+namespace salad
+{
+    public class Logger
+    {
+        internal static MelonLogger.Instance _Log = new MelonLogger.Instance(typeof(Logger).Namespace, ConsoleColor.Magenta);
+
+        public static void Log(object obj)
+        {
+            _Log.Msg(obj);
+        }
+
+        public static void LogError(object obj)
+        {
+            _Log.Error(obj);
+        }
+    }
+}
---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: MelonInfo(typeof(salad.MyMod), "salad", "0.0.1a", "Kin")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
+
+namespace salad
+{
+    public class MyMod : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            [DllImport("user32.dll", EntryPoint = "SetWindowText")]
+            static extern bool SetWindowText(System.IntPtr hwnd, System.String lpString);
+            [DllImport("user32.dll", EntryPoint = "FindWindow")]
+            static extern System.IntPtr FindWindow(System.String className, System.String windowName);
+            System.IntPtr windowPtr = FindWindow(null, "ChilloutVR");
+            SetWindowText(windowPtr, "ChilloutVR - salad");
+            Logger.Log("Get salad'd");
+        }
+    }
+}
